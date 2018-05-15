8d018974b0f2267a61ab0ae740e7c9d4e55375e9
DiegoFioretti <diego_fioretti_97@hotmail.com>
2018-05-15 17:00:08 -0300

    Implementaciones iniciales
    +Añadido .gitignore.
    +Añadido archivos para Allegro.
    +Configurado propiedades para funcionamiento de Allegro, Debug utiliza librerias dinamicas y Release utiliza librerias estaticas.

---
+++ Pong/Main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <allegro5\allegro.h>
+#include <allegro5\allegro_image.h>
+#ifdef ALLEGRO_STATICLINK
+#include <libpng16\png.h>
+#include <zlib.h>
+#include <jpeglib.h>
+#endif
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+
+}
